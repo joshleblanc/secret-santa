22e8e95f603568bbe3ffb5f2b1e0bdf754c0796a
Josh LeBlanc <jleblanc@omics.com>
2019-10-29 15:57:12 -0300

    remove user file until I actually need it

--- imports/api/users.js
+++
@@ -1,5 +0,0 @@
-import * as yup from 'yup';
-
-export const schema = yup.object().shape({
-    apiKey: yup.string().matches(/[A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{20}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12}/, "Not a valid api key")
-});
