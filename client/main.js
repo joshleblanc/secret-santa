4df9e64e3c53214845c28cdcf29727c0175a849b
Joshua LeBlanc <me@jleb.ca>
2019-10-31 18:21:22 -0300

    remove ssr

--- client/main.js
+++ client/main.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { hydrate } from 'react-dom';
-import { FastRender } from 'meteor/staringatlights:fast-render';
+import { render } from 'react-dom';
 import '../imports/api/users';
+import App from '../imports/ui/entry_points/ClientEntryPoint';
 
-FastRender.onPageLoad(async sink => {
-  const App = (await import('../imports/ui/entry_points/ClientEntryPoint')).default;
-  hydrate(<App />, document.getElementById('react-target'))
+Meteor.startup(() => {
+  render(<App />, document.getElementById('react-target'))
 });
--- server/main.js
+++ server/main.js
@@ -1,7 +1,4 @@
-import {FastRender} from 'meteor/staringatlights:fast-render';
-import ServerStyleSheets from "@material-ui/styles/ServerStyleSheets";
 import React from "react";
-import ReactDOMServer from 'react-dom/server'
 import '../imports/api/groups';
 import '../imports/api/users';
 import './api';
@@ -16,19 +13,3 @@ ServiceConfiguration.configurations.upsert(
       }
   }
 );
-
-FastRender.onPageLoad(async sink => {
-    const ServerEntryPoint = (await import('../imports/ui/entry_points/ServerEntryPoint')).default;
-    const sheets = new ServerStyleSheets();
-
-    const html = ReactDOMServer.renderToString(
-        sheets.collect(
-            <ServerEntryPoint location={sink.request.url} />
-        ),
-    );
-
-    const css = sheets.toString();
-
-    sink.appendToHead(`<style id="jss-server-side">${css}</style>`);
-    sink.renderIntoElementById('react-target', html);
-});
