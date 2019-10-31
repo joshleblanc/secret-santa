1bc9eb15ae9572c14f949a3a7917a66c3079dafa
Joshua LeBlanc <me@jleb.ca>
2019-10-31 14:03:25 -0300

    mildly improve index page

--- imports/ui/pages/Index.jsx
+++ imports/ui/pages/Index.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import Grid from "@material-ui/core/Grid";
+import {Typography} from "@material-ui/core";
+import PaddedPaper from "../components/PaddedPaper";
 
 export default class extends React.Component {
     render() {
-        return "Index";
+        return(
+          <Grid container spacing={2} justify={"center"}>
+              <Grid item xs={12} md={6}>
+                  <PaddedPaper>
+                      <Typography variant={"h4"}>Hello!</Typography>
+                      <Typography>
+                          Login to get started
+                      </Typography>
+                  </PaddedPaper>
+              </Grid>
+          </Grid>
+        )
     }
 }
