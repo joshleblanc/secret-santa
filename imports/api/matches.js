25adad655a924dfa330710c33ed7052e2faf25b1
Joshua LeBlanc <me@jleb.ca>
2019-11-02 11:09:45 -0300

    add matches collection

---
+++ imports/api/matches.js
@@ -0,0 +1,34 @@
+import * as yup from 'yup';
+
+export const Matches = new Mongo.Collection('matches', { idGeneration: "MONGO" });
+
+export const schema = yup.object().shape({
+  gifter: yup.string().required(),
+  receiver: yup.string().required(),
+  groupId: yup.string().required()
+});
+
+if(Meteor.isServer) {
+  Meteor.publish('match', function(groupId, discordUserId) {
+    const matches = Matches.find({
+      gifter: discordUserId,
+      groupId: groupId
+    });
+
+    const match = Matches.findOne({
+      gifter: discordUserId,
+      groupId: groupId
+    });
+
+    const users = Meteor.users.find({
+      "services.discord.id": match.receiver
+    }, {
+      fields: {
+        avatar: 1,
+        discordID: 1,
+        email: 1
+      }
+    });
+    return [matches, users];
+  });
+}
