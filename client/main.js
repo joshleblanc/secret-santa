ef3e7775a40b5ba9792f3bcba5ede916f48c4960
Joshua LeBlanc <me@jleb.ca>
2019-11-02 11:45:15 -0300

    remove old ssr files

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import '../imports/api/users';
-import App from './ui/entry_points/ClientEntryPoint';
+import App from './ui/App';
 
 Meteor.startup(() => {
   render(<App />, document.getElementById('react-target'))
--- client/ui/App.js
+++ client/ui/App.js
@@ -1,11 +1,31 @@
-import React, { Component } from 'react';
-import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './components/Routes';
+import MomentUtils from "@date-io/moment";
+import {createGenerateClassName, StylesProvider, ThemeProvider} from "@material-ui/styles";
+import theme from "./lib/theme";
+import {MuiPickersUtilsProvider} from "@material-ui/pickers";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
-export default class App extends Component {
-
-  render() {
-    return (
-      null
-    );
-  }
+export default () => {
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }, []);
+  return(
+    <MuiPickersUtilsProvider utils={MomentUtils}>
+      <StylesProvider
+        generateClassName={createGenerateClassName()}
+      >
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <BrowserRouter>
+            <Routes />
+          </BrowserRouter>
+        </ThemeProvider>
+      </StylesProvider>
+    </MuiPickersUtilsProvider>
+  )
 }
