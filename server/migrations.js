89c52066eed457bcb42b3cee65da9be954e9511e
Joshua LeBlanc <me@jleb.ca>
2019-11-02 11:10:18 -0300

    add migrations to fix some problems

--- server/migrations.js
+++ server/migrations.js
@@ -6,8 +6,10 @@ Migrations.add({
     const groups = Groups.find({}).fetch();
     groups.forEach(g => {
       Groups.update({ _id: g._id }, {
-        startDate: new Date(g.startDate),
-        endDate: new Date(g.endDate)
+        $set: {
+          startDate: new Date(g.startDate),
+          endDate: new Date(g.endDate)
+        }
       })
     })
   },
@@ -15,8 +17,64 @@ Migrations.add({
     const groups = Groups.find({}).fetch();
     groups.forEach(g => {
       Groups.update({ _id: g._id }, {
-        startDate: g.startDate.toString(),
-        endDate: g.startDate.toString()
+        $set: {
+          startDate: g.startDate.toString(),
+          endDate: g.startDate.toString()
+        }
+      })
+    })
+  }
+});
+
+Migrations.add({
+  version: 2,
+  up: function() {
+    const groups = Groups.find({}).fetch();
+    groups.forEach(g => {
+      Groups.update({ _id: g._id }, {
+        $set: {
+          hasMatches: false
+        }
+      })
+    })
+  },
+  down: function() {
+    const groups = Groups.find({}).fetch();
+    groups.forEach(g => {
+      Groups.update({ _id: g._id }, {
+        $unset: {
+          hasMatches: ""
+        }
+      })
+    })
+  }
+});
+
+Migrations.add({
+  version: 3,
+  up: function() {
+    const users = Meteor.users.find({}).fetch();
+    users.forEach(u => {
+      Groups.update({ _id: u._id }, {
+        $set: {
+          avatar: u.services.discord.avatar,
+          discordId: u.services.discord.id,
+          email: u.services.discord.email,
+          username: u.services.discord.username
+        }
+      })
+    })
+  },
+  down: function() {
+    const users = Meteor.users.find({}).fetch();
+    users.forEach(u => {
+      Groups.update({ _id: u._id }, {
+        $unset: {
+          avatar: "",
+          discordId: "",
+          email: "",
+          username: ""
+        }
       })
     })
   }
