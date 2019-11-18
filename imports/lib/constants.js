3972ffce799ce7513004aa0187401df9de878299
Shmiddty <corey.wade.hart@gmail.com>
2019-11-18 16:21:02 -0700

    Update constants.js
    The sizes were discriminatory against Attack Helicopters.

--- imports/lib/constants.js
+++ imports/lib/constants.js
@@ -1,7 +1,15 @@
 export const shirtSizes = {
+    "z": "+[]",
+    "ta": "Tardigrade",
+    "t": "Toddler",
     "xs": "Extra Small",
     "s": "Small",
     "m": "Medium",
     "l": "Large",
-    "xl": "Extra Large"
+    "xl": "Extra Large",
+    "ms": "Mythical Stag",
+    "ah": "Attack Helicopter",
+    "ds": "That's no moon..."
+    "ag": "Andromeda",
+    "inf": "1/0"
 };
