2021c11adfa68072915a541a43abc41ad31a30e6
Joshua LeBlanc <me@jleb.ca>
2019-12-13 19:29:41 -0400

    hide message if logged in

--- client/ui/pages/Index.jsx
+++ client/ui/pages/Index.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Container from "../components/Container";
-import { SignUpEndings } from '../components/SignUpEndings';
+import {SignUpEndings} from '../components/SignUpEndings';
+import {autorun} from 'meteor/cereal:reactive-render';
 
+@autorun
 export default class extends React.Component {
-    render() {
-        return(
-          <>
-            <Container>
-              <Typography variant={"h4"}>Hello!</Typography>
-              <Typography>
-                Login to get started
-              </Typography>
-            </Container>
-            <Container>
-              <Typography variant="h4">Approaching Deadlines</Typography>
-              <br />
-              <Typography variant="h5">Sign Ups Ending</Typography>
-              <SignUpEndings />
-            </Container>
-          </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <Container>
+          <Typography variant={"h4"}>Hello!</Typography>
+          {
+            !Meteor.userId() && <Typography>
+              Login to get started
+            </Typography>
+          }
+        </Container>
+        <Container>
+          <Typography variant="h4">Approaching Deadlines</Typography>
+          <br/>
+          <Typography variant="h5">Sign Ups Ending</Typography>
+          <SignUpEndings/>
+        </Container>
+      </>
+    )
+  }
 }
