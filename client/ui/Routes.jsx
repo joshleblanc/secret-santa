bd75ea5ca97434ae1162fabbd87f5e17450122eb
Joshua LeBlanc <me@jleb.ca>
2019-12-14 11:51:15 -0400

    fix loading bar when not logged in

--- client/ui/Routes.jsx
+++ client/ui/Routes.jsx
@@ -7,17 +7,26 @@ import Group from "./pages/Group";
 import {Route, Switch} from "react-router-dom";
 import Messages from "./pages/Messages";
 import Message from './pages/Message';
+import { autorun } from 'meteor/cereal:reactive-render';
 
-export default () => {
-  return(
-    <Switch>
-      <Route exact path="/" component={Index} />
-      <Route exact path="/profile" component={Profile} />
-      <Route exact path="/groups" component={Groups} />
-      <Route exact path="/groups/add" component={AddGroup} />
-      <Route exact path="/groups/:id" component={Group} />
-      <Route exact path="/messages" component={Messages} />
-      <Route exact path="/messages/:id" component={Message} />
-    </Switch>
-  )
+@autorun
+export default class extends React.Component {
+  render() {
+    const user = Meteor.user();
+    if(user) {
+      return(
+        <Switch>
+          <Route exact path="/" component={Index} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/groups" component={Groups} />
+          <Route exact path="/groups/add" component={AddGroup} />
+          <Route exact path="/groups/:id" component={Group} />
+          <Route exact path="/messages" component={Messages} />
+          <Route exact path="/messages/:id" component={Message} />
+        </Switch>
+      )
+    } else {
+      return <Index />
+    }
+  }
 };
