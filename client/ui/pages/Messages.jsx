895b5316afd0903d40a4c3f334cf19ab5a41e95a
Joshua LeBlanc <me@jleb.ca>
2019-12-14 10:32:25 -0400

    add badge to message item if unread

--- client/ui/pages/Messages.jsx
+++ client/ui/pages/Messages.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {autorun} from 'meteor/cereal:reactive-render';
-import {LinearProgress} from "@material-ui/core";
+import {LinearProgress, withStyles} from "@material-ui/core";
 import {Matches} from '/imports/api/matches';
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
@@ -10,11 +10,20 @@ import PaddedPaper from "../components/PaddedPaper";
 import {Link} from "react-router-dom";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
+import Badge from "@material-ui/core/Badge";
 
+const styles = theme => ({
+  badge: {
+    margin: theme.spacing(2, 1)
+  }
+});
+
+@withStyles(styles)
 @autorun
 export default class extends React.Component {
 
   render() {
+    const { classes } = this.props;
     const matchesSubscription = Meteor.subscribe('matches', Meteor.userId());
     const groupsSubscription = Meteor.subscribe('groups', Meteor.userId());
     if (!matchesSubscription.ready() || !groupsSubscription.ready()) {
@@ -35,16 +44,32 @@ export default class extends React.Component {
               const group = Groups.findOne({_id: m.groupId});
               let secondary;
               console.log(m.receiver, user.discordId);
-              if(m.receiver === user.discordId) {
+              if (m.receiver === user.discordId) {
                 secondary = "Your secret santa messaged you!"
               } else {
                 secondary = "You message your recipient!"
               }
+              const unreadMessages = user.unreadMessages || [];
+              let hasUnreadMessage = unreadMessages.map(u => u.toHexString()).includes(m._id.toHexString());
+              console.log(user.unreadMessages, m._id);
+              console.log(user.unreadMessages && user.unreadMessages.includes(m._id));
               return (
                 <React.Fragment key={m._id.toHexString()}>
-                  <ListItem button component={Link} key={m._id.toHexString()} to={`/messages/${m._id.toHexString()}`}>
-                    <ListItemText primary={group.name} secondary={secondary}/>
+                  <Badge
+                    color={"primary"}
+                    badgeContent={hasUnreadMessage ? "" : 0}
+                    variant={"dot"}
+                    anchorOrigin={{ vertical: 'top', horizontal: 'left'}}
+                    classes={classes}
+                  >
+                  <ListItem button component={Link} to={`/messages/${m._id.toHexString()}`}>
+                      <ListItemText primary={
+
+                          group.name
+                      } secondary={secondary}/>
                   </ListItem>
+                  </Badge>
+
                   <Divider/>
                 </React.Fragment>
               )
