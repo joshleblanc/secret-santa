c52c692a440c270a1ad1a5767f72a78db7a17e15
Joshua LeBlanc <me@jleb.ca>
2019-12-14 10:19:02 -0400

    notification when there are unread messages

--- client/ui/components/drawer/MessagesDrawerItem.jsx
+++ client/ui/components/drawer/MessagesDrawerItem.jsx
@@ -5,18 +5,28 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Badge from "@material-ui/core/Badge";
+import { autorun } from 'meteor/cereal:reactive-render';
+import {LinearProgress} from "@material-ui/core";
 
-export default () => {
-  return (
+@autorun
+export default class extends React.Component {
+  render() {
+    const user = Meteor.user();
+    if(!user) {
+      return <LinearProgress />
+    }
+    console.log(user);
+    const unread = user.unreadMessages;
+    return (
+      <ListItem button component={Link} to={"/messages"}>
+        <ListItemIcon>
+          <Badge color={"primary"} badgeContent={unread ? unread.length : 0} variant={"dot"}>
+            <EmailIcon/>
+          </Badge>
+        </ListItemIcon>
+        <ListItemText>Messages</ListItemText>
+      </ListItem>
 
-    <ListItem button component={Link} to={"/messages"}>
-      <ListItemIcon>
-        <Badge color={"primary"} badgeContent={""} variant={"dot"}>
-          <EmailIcon/>
-        </Badge>
-      </ListItemIcon>
-      <ListItemText>Messages</ListItemText>
-    </ListItem>
-
-  )
+    )
+  }
 }
