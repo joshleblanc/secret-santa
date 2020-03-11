a0748555f999b5c6a6a3c68d249d5e4bc1295d37
Joshua LeBlanc <jleblanc@omics.com>
2020-03-11 09:31:41 -0300

    improve mobile experience on groups pages

--- client/ui/components/Header.jsx
+++ client/ui/components/Header.jsx
@@ -1,35 +1,42 @@
 import React from 'react';
 import {withStyles} from "@material-ui/styles";
 import Typography from "@material-ui/core/Typography";
-import Grow from "./Grow";
 import Button from "@material-ui/core/Button";
 import {Link} from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
 
 const styles = theme => ({
     header: {
         display: 'flex',
         marginBottom: theme.spacing(2)
+    },
+    buttonContainer: {
+      textAlign: "end"
     }
 });
 
 @withStyles(styles)
 export default class Header extends React.Component {
     render() {
-        const { classes, title, to, linkLabel } = this.props;
+        const { classes, title, to } = this.props;
         return(
-            <div className={classes.header}>
-                <Typography variant={"h4"}>{title}</Typography>
-                <Grow/>
-                <Button
-                    size="small"
-                    component={Link}
-                    variant="contained"
-                    to={to}
-                    color={"secondary"}
-                >
-                    {linkLabel}
-                </Button>
-            </div>
+          <Grid container>
+            <Grid item xs={12} sm={10}>
+              <Typography variant={"h4"}>{title}</Typography>
+            </Grid>
+            <Grid item xs={12} sm={2} className={classes.buttonContainer}>
+              <Button
+                size="small"
+                component={Link}
+                fullWidth
+                variant="contained"
+                to={to}
+                color={"primary"}
+              >
+                Create
+              </Button>
+            </Grid>
+          </Grid>
         )
     }
 }
