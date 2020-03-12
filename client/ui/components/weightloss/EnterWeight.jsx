8cf19c8bbf633dcc0c33485cc9d5181d3e3ba32f
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-03-12 12:41:52 -0300

    format file

--- client/ui/components/weightloss/EnterWeight.jsx
+++ client/ui/components/weightloss/EnterWeight.jsx
@@ -15,118 +15,118 @@ import {WeightGroups} from "../../../../imports/api/weight_groups";
 import Grid from "@material-ui/core/Grid";
 
 const initialValues = {
-  weight: "",
-  measurement: "lbs"
+    weight: "",
+    measurement: "lbs"
 };
 
 const styles = theme => ({
-  container: {
-    display: 'flex'
-  },
-  button: {
-    margin: theme.spacing(1),
-    alignSelf: "flex-end"
-  }
+    container: {
+        display: 'flex'
+    },
+    button: {
+        margin: theme.spacing(1),
+        alignSelf: "flex-end"
+    }
 });
 
 @withStyles(styles)
 @withSnackbar
 @autorun
 export default class EnterWeight extends React.Component {
-  state = {
-    entriesDialogOpen: false
-  };
+    state = {
+        entriesDialogOpen: false
+    };
 
-  handleSubmit = (values, {setSubmitting}) => {
-    const {enqueueSnackbar, groupId} = this.props;
-    setSubmitting(true);
-    Meteor.call('user.addWeight', groupId, values.weight, values.measurement, err => {
-      if (err) {
-        enqueueSnackbar("Something went wrong D:", {variant: "error"});
-      } else {
-        enqueueSnackbar("WeightGroup added!", {variant: "success"});
-      }
-      setSubmitting(false);
-    });
-  };
+    handleSubmit = (values, {setSubmitting}) => {
+        const {enqueueSnackbar, groupId} = this.props;
+        setSubmitting(true);
+        Meteor.call('user.addWeight', groupId, values.weight, values.measurement, err => {
+            if (err) {
+                enqueueSnackbar("Something went wrong D:", {variant: "error"});
+            } else {
+                enqueueSnackbar("WeightGroup added!", {variant: "success"});
+            }
+            setSubmitting(false);
+        });
+    };
 
-  openEntriesDialog = () => {
-    this.setState({entriesDialogOpen: true});
-  };
+    openEntriesDialog = () => {
+        this.setState({entriesDialogOpen: true});
+    };
 
-  closeEntriesDialog = () => {
-    this.setState({entriesDialogOpen: false});
-  };
+    closeEntriesDialog = () => {
+        this.setState({entriesDialogOpen: false});
+    };
 
-  join = () => {
-    Meteor.call("weight_groups.join", this.props.groupId);
-  };
+    join = () => {
+        Meteor.call("weight_groups.join", this.props.groupId);
+    };
 
-  leave = () => {
-    Meteor.call("weight_groups.leave", this.props.groupId);
-  };
+    leave = () => {
+        Meteor.call("weight_groups.leave", this.props.groupId);
+    };
 
-  render() {
-    const {classes, groupId} = this.props;
-    const ready = Meteor.subscribe('weight_group', groupId).ready();
-    if (!ready) {
-      return <LinearProgress/>
+    render() {
+        const { groupId } = this.props;
+        const ready = Meteor.subscribe('weight_group', groupId).ready();
+        if (!ready) {
+            return <LinearProgress/>
+        }
+        const group = WeightGroups.findOne(new Mongo.ObjectID(groupId));
+        const isMember = group.userIds.includes(Meteor.userId());
+
+        return (
+            <PaddedPaper>
+                <EntriesDialog open={this.state.entriesDialogOpen} onClose={this.closeEntriesDialog}/>
+                <Formik initialValues={initialValues} validationSchema={addWeightSchema} onSubmit={this.handleSubmit}>
+                    <Form>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={3}>
+                                <Field
+                                    label={"Current weight"}
+                                    name={"weight"}
+                                    component={TextField}
+                                    margin={"normal"}
+                                    fullWidth
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={3}>
+                                <Field
+                                    label={"Measurement"}
+                                    name={"measurement"}
+                                    component={TextField}
+                                    margin={"normal"}
+                                    fullWidth
+                                    select
+                                >
+                                    <MenuItem value={"kg"}>Kilograms</MenuItem>
+                                    <MenuItem value={"lbs"}>Pounds</MenuItem>
+                                    <MenuItem value={'stone'}>Stones</MenuItem>
+                                </Field>
+                            </Grid>
+                        </Grid>
+                        <Grid container spacing={1}>
+                            <Grid item xs={12} sm={3}>
+                                <Button fullWidth type={"submit"} color="primary"
+                                        variant={"contained"}>Submit</Button>
+                            </Grid>
+                            <Grid item xs={12} sm={3}>
+                                <Button color="secondary" variant={"contained"} fullWidth
+                                        onClick={this.openEntriesDialog}>Manage Entries</Button>
+                            </Grid>
+                            <Grid item xs={12} sm={3}>
+                                {
+                                    isMember
+                                        ? <Button fullWidth color="secondary" variant={"contained"}
+                                                  onClick={this.leave}>Leave</Button>
+                                        : <Button fullWidth color="secondary" variant={"contained"}
+                                                  onClick={this.join}>Join</Button>
+                                }
+                            </Grid>
+                        </Grid>
+                    </Form>
+                </Formik>
+            </PaddedPaper>
+        )
     }
-    const group = WeightGroups.findOne(new Mongo.ObjectID(groupId));
-    const isMember = group.userIds.includes(Meteor.userId());
-
-    return (
-      <PaddedPaper>
-        <EntriesDialog open={this.state.entriesDialogOpen} onClose={this.closeEntriesDialog}/>
-        <Formik initialValues={initialValues} validationSchema={addWeightSchema} onSubmit={this.handleSubmit}>
-          <Form>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={3}>
-                <Field
-                  label={"Current weight"}
-                  name={"weight"}
-                  component={TextField}
-                  margin={"normal"}
-                  fullWidth
-                />
-              </Grid>
-              <Grid item xs={12} sm={3}>
-                <Field
-                  label={"Measurement"}
-                  name={"measurement"}
-                  component={TextField}
-                  margin={"normal"}
-                  fullWidth
-                  select
-                >
-                  <MenuItem value={"kg"}>Kilograms</MenuItem>
-                  <MenuItem value={"lbs"}>Pounds</MenuItem>
-                  <MenuItem value={'stone'}>Stones</MenuItem>
-                </Field>
-              </Grid>
-            </Grid>
-            <Grid container spacing={1}>
-              <Grid item xs={12} sm={3}>
-                <Button fullWidth type={"submit"} color="primary"
-                        variant={"contained"}>Submit</Button>
-              </Grid>
-              <Grid item xs={12} sm={3}>
-                <Button  color="secondary" variant={"contained"} fullWidth
-                        onClick={this.openEntriesDialog}>Manage Entries</Button>
-              </Grid>
-              <Grid item xs={12} sm={3}>
-                {
-                  isMember
-                    ? <Button fullWidth color="secondary" variant={"contained"}
-                              onClick={this.leave}>Leave</Button>
-                    : <Button fullWidth color="secondary" variant={"contained"}
-                              onClick={this.join}>Join</Button>
-                }
-              </Grid>
-            </Grid>
-          </Form>
-        </Formik>
-      < /PaddedPaper>
-    )
-  }
 }
