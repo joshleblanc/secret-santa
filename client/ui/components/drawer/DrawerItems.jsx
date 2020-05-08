a2878b10165861f75208e0aa72f11a06807ac685
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-05-08 08:24:23 -0300

    remove unused code

--- client/ui/components/drawer/DrawerItems.jsx
+++ client/ui/components/drawer/DrawerItems.jsx
@@ -6,28 +6,12 @@ import Divider from "@material-ui/core/Divider";
 import {Link} from "react-router-dom";
 import { autorun } from 'meteor/cereal:reactive-render';
 import List from "@material-ui/core/List";
-import withStyles from '@material-ui/core/styles/withStyles';
 import { routes } from '../../lib/constants';
 import AuthenticatedDrawerItems from "./AuthenticatedDrawerItems";
-import ThemeIcon from "@material-ui/icons/WbSunny";
-import {AppStoreContext} from "../../stores/AppStore";
 
-const styles = theme => ({
-  toolbar: theme.mixins.toolbar,
-  nested: {
-    paddingLeft: theme.spacing(4),
-  },
-});
-
-@withStyles(styles)
 @autorun
 export default class extends React.Component {
-  static contextType = AppStoreContext;
-
   render() {
-    const { classes } = this.props;
-    const { toggleTheme } = this.context;
-
     return(
       <div>
         <Divider />
@@ -42,12 +26,6 @@ export default class extends React.Component {
               </ListItem>
             ))
           }
-          <ListItem button onClick={toggleTheme}>
-            <ListItemIcon>
-              <ThemeIcon />
-            </ListItemIcon>
-            <ListItemText primary="Toggle Theme" />
-          </ListItem>
         </List>
         <AuthenticatedDrawerItems />
       </div>
