199cc9279b7e163d41bec33f8442030939be88a0
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-05-11 18:35:47 -0300

    add enter bells component to bell list page

--- client/ui/pages/BellGroupList.jsx
+++ client/ui/pages/BellGroupList.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import {BaseGroupList} from "../components/groups/BaseGroupList";
 import {BellGroups} from "../../../imports/api/bell_groups";
+import {EnterBells} from "../components/bells/EnterBells";
 
 export const BellGroupList = () => {
   return(
-    <BaseGroupList
-      subscription={"bell_groups"}
-      collection={BellGroups}
-      addLinkLabel={"Create Turnip Group"}
-      addUrl={"/bells/groups/add"}
-      createGroupUrl={id => `/bells/groups/${id.toHexString()}`}
-      deleteMethod={"bell_groups.delete"}
-      title={"Bell Groups"}
-    />
+    <>
+      <EnterBells />
+      <BaseGroupList
+        subscription={"bell_groups"}
+        collection={BellGroups}
+        addLinkLabel={"Create Turnip Group"}
+        addUrl={"/bells/groups/add"}
+        createGroupUrl={id => `/bells/groups/${id.toHexString()}`}
+        deleteMethod={"bell_groups.delete"}
+        title={"Bell Groups"}
+      />
+    </>
   )
 }
