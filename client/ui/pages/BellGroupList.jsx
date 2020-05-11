cd48b611d74723fdf293a967e8def207dccdea5d
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-05-11 19:50:16 -0300

    add spacing to bell group list

--- client/ui/pages/BellGroupList.jsx
+++ client/ui/pages/BellGroupList.jsx
@@ -2,20 +2,25 @@ import React from 'react';
 import {BaseGroupList} from "../components/groups/BaseGroupList";
 import {BellGroups} from "../../../imports/api/bell_groups";
 import {EnterBells} from "../components/bells/EnterBells";
+import Grid from "@material-ui/core/Grid";
 
 export const BellGroupList = () => {
   return(
-    <>
-      <EnterBells />
-      <BaseGroupList
-        subscription={"bell_groups"}
-        collection={BellGroups}
-        addLinkLabel={"Create Turnip Group"}
-        addUrl={"/bells/groups/add"}
-        createGroupUrl={id => `/bells/groups/${id.toHexString()}`}
-        deleteMethod={"bell_groups.delete"}
-        title={"Turnip Groups"}
-      />
-    </>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <EnterBells />
+      </Grid>
+      <Grid item xs={12}>
+        <BaseGroupList
+          subscription={"bell_groups"}
+          collection={BellGroups}
+          addLinkLabel={"Create Turnip Group"}
+          addUrl={"/bells/groups/add"}
+          createGroupUrl={id => `/bells/groups/${id.toHexString()}`}
+          deleteMethod={"bell_groups.delete"}
+          title={"Turnip Groups"}
+        />
+      </Grid>
+    </Grid>
   )
 }
