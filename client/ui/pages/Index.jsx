183db1f9d3148243edf211daa455d86f50cd12e9
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-06-05 20:48:12 -0300

    fix spacing on index page

--- client/ui/pages/Index.jsx
+++ client/ui/pages/Index.jsx
@@ -3,22 +3,27 @@ import Typography from '@material-ui/core/Typography';
 import {autorun} from 'meteor/cereal:reactive-render';
 import PaddedPaper from "../components/PaddedPaper";
 import Dashboard from "../components/Dashboard";
+import Grid from "@material-ui/core/Grid";
 
 @autorun
 export default class extends React.Component {
   render() {
     return (
-      <>
-        <PaddedPaper>
-          <Typography variant={"h4"}>Hello!</Typography>
-          {
-            !Meteor.userId() && <Typography>
-              Login to get started
-            </Typography>
-          }
-        </PaddedPaper>
-        <Dashboard />
-      </>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <PaddedPaper>
+            <Typography variant={"h4"}>Hello!</Typography>
+            {
+              !Meteor.userId() && <Typography>
+                Login to get started
+              </Typography>
+            }
+          </PaddedPaper>
+        </Grid>
+        <Grid item xs={12}>
+          <Dashboard />
+        </Grid>
+      </Grid>
     )
   }
 }
